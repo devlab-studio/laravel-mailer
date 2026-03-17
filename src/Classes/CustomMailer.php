100a29a18d61e98a4f1602df2fa8a45d6ebd6736
Roberto <rsimonru@gmail.com>
2026-03-17 17:23:33 +0100

    Fixes

--- src/Classes/CustomMailer.php
+++ src/Classes/CustomMailer.php
@@ -7,25 +7,23 @@ use Devlab\LaravelMailer\Models\EmailSender;
 class CustomMailer {
 
     public static function setMailer($from) {
-        $default_mailer = ($from != config('devlab.MAIL_FROM_ADDRESS')) ? false : true;
         $mailer = 'smtp';
 
-        if (!$default_mailer) {
-            $sender = EmailSender::where('address', $from)->get()->first();
-            if ($sender) {
-                config([
-                    'mail.mailers.custom'.$sender->id => [
-                        'transport' => 'smtp',
-                        'host' => $sender->server,
-                        'port' => $sender->port,
-                        'username' => decrypt($sender->auth_user),
-                        'password' => decrypt($sender->auth_password),
-                        'encryption' => $sender->auth_protocol,
-                    ],
-                ]);
-                $mailer = 'custom'.$sender->id;
-            }
+        $sender = EmailSender::where('address', $from)->get()->first();
+        if ($sender) {
+            config([
+                'mail.mailers.custom'.$sender->id => [
+                    'transport' => 'smtp',
+                    'host' => $sender->server,
+                    'port' => $sender->port,
+                    'username' => $sender->auth_user,
+                    'password' => decrypt($sender->auth_password),
+                    'encryption' => $sender->auth_protocol,
+                ],
+            ]);
+            $mailer = 'custom'.$sender->id;
         }
+
         return $mailer;
     }
 }
